4b3598d7f724da3582bbf7235dcf2aec8aeb081b
Cutiemango <h7a4n1k@gmail.com>
2024-11-24 21:11:30 +0800

    feat: neural network basic structure

---
+++ neural_network/neural_network.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "activation_func.h"
+
+#include <vector>
+#include <memory>
+
+class Layer {
+public:
+    Layer(int input_size, int output_size, const std::string& activation_name);
+    Layer(std::ifstream& file);
+    ~Layer() = default;
+
+    Layer(Layer&& other) noexcept;
+    Layer& operator=(Layer&& other) noexcept;
+
+    Layer(const Layer&) = delete;
+    Layer& operator=(const Layer&) = delete;
+
+    void forward(float* input, float* output);
+    void set_weights(float* new_weights);
+    void save(std::ofstream& file);
+
+private:
+    int input_size;
+    int output_size;
+    Activation activation;
+    float(*activation_func)(float);
+    std::unique_ptr<float[]> weights; // (input_size + 1) * output_size, bias in the last column
+};
+
+class MLP {
+public:
+    MLP(int* layer_sizes, int num_layers, const std::string& layer_activation, const std::string& output_activation);
+    MLP(const std::string& filename);  // Load weights from file
+
+    void forward(float* input, float* output);
+    void save(const std::string& filename);
+
+private:
+    std::vector<Layer> layers;
+};
