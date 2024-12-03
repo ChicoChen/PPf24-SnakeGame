51cb838b4d95e20cc2419c18f28420c39b33c07f
cseslowpoke <cseslowpoke@gmail.com>
2024-12-04 01:11:52 +0800

    fix direction checking in get_feature

--- lib/game/game.cpp
+++ lib/game/game.cpp
@@ -113,10 +113,10 @@ std::vector<float> Game::get_features() const {
     features.push_back(dist_to_self);
     features.push_back(dist_to_wall);
   }
-  for (int i = 0; i < 4; i++) {
+  for (int i = 1; i <= 4; i++) {
     features.push_back(snake.head_direction() == i);
   }
-  for (int i = 0; i < 4; i++) {
+  for (int i = 1; i <= 4; i++) {
     features.push_back(snake.tail_direction() == i);
   }
   return features; 
