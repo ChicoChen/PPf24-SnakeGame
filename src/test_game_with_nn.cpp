9d4e92f0734907907940884d162d923086092502
cseslowpoke <cseslowpoke@gmail.com>
2024-12-12 11:14:22 +0800

    update model loading and adjust sleep duration in test_game_with_nn

--- src/test_game_with_nn.cpp
+++ src/test_game_with_nn.cpp
@@ -1,6 +1,8 @@
 #include "nn/neural_network.h"
 #include "game/game.h"
 #include <chrono>
+#include <iostream>
+#include <string>
 #include <thread>
 
 Direction get_direction(Game& game, MLP& mlp) {
@@ -14,12 +16,18 @@ Direction get_direction(Game& game, MLP& mlp) {
 }
 
 int main() {
-  MLP mlp("model.bin");
+  srand(time(NULL));
+  std::string model_name = "demo_1000_100";
+  MLP mlp(model_name);
+  // printf("Model loaded\n");
   Game game = Game();
   Direction dir;
   do {
     game.dump();
+    printf("Direction: %d\n", dir);
     dir = get_direction(game, mlp);
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
   } while (game.run(dir));
+  std::cout << game.calculate_fitness() << std::endl;
 }
