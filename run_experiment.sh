089618d8b2a28332e53c3814b29ed287f64c2d7c
ChicoChen <chin2839211@gmail.com>
2024-12-16 20:23:29 +0800

    add experiment scripts, and add directory models/experiments

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-# Loop to run the program 5 times with different parameters
+build_dir="./build"
+model_dir="./models/experiments"
+
+# Serial runs
 for i in {1..5}; do
-    modelName="exp${i}_1024_1500"
-    threadNum=8
-    ./test_ga "$test_ga" $threadNum
-    echo "Run $i completed with parameter: $modelName"
+    modelFile="exp_serial${i}_1024_1500"
+    modelLog="$modelFile.log"
+    taskset -c 0 "${build_dir}/test_serial" "$modelFile"
+
+    mv "${modelFile}" "$model_dir"
+    mv "${modelLog}" "$model_dir"
+    echo "Run $i completed with parameter: $modelFile"
+done
+
+# Parallel runs
+threadNum=8
+for i in {1..5}; do
+    modelFile="exp_openmp${i}_1024_1500"
+    modelLog="$modelFile.log"
+
+    taskset -c 0-7 "${build_dir}/test_openmp" "$modelFile" $threadNum
+
+    mv "$modelFile" "$model_dir"
+    mv "$modelLog" "$model_dir"
+    echo "Run $i completed with parameter: $modelFile, $threadNum"
 done
 
 echo "All runs completed."
